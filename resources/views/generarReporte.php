a5749714c7a6a61685a6c58c9d00abf56d068ea4
Johny Marin Tobar <gonzalesdario754@gmail.com>
2021-11-16 15:15:19 -0500

    Organizando la tabla de reportes

--- resources/views/generarReporte.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-$mpdf = new \Mpdf\Mpdf();
-$mpdf -> WriteHTML('<h1>PDF desde mpdf</h1>');
-
-$mpdf -> OutPut('pdfNuevo.pdf');
-echo ('Todo correcto');
